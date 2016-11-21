6a761246f20c59c9941c39ad0378159c7d840acf
nunes <nunes@JNA>
2016-11-21 18:19:31 -0200

    Alteracoes.

--- src/main/java/br/com/nunes/mercado/bean/ProdutoBean.java
+++ src/main/java/br/com/nunes/mercado/bean/ProdutoBean.java
@@ -1,11 +1,16 @@
 package br.com.nunes.mercado.bean;
 
 import java.io.Serializable;
+import java.util.List;
 
-import javax.faces.application.FacesMessage;
+import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
-import javax.faces.context.FacesContext;
+
+import org.omnifaces.util.Messages;
+
+import br.com.nunes.mercado.dao.ProdutoDAO;
+import br.com.nunes.mercado.domain.Produto;
 
 @ManagedBean
 @ViewScoped
@@ -13,14 +18,42 @@ public class ProdutoBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 
-	public void salvar() {
+	private Produto produto;
+	private ProdutoDAO produtoDAO;
 
-		FacesContext context = FacesContext.getCurrentInstance();
+	private List<Produto> produtos;
 
-		FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Resumo do aviso flutuante",
-				"Aviso flutuante completo");
+	public Produto getProduto() {
+		return produto;
+	}
 
-		context.addMessage("mensagem", msg);
+	public void setProduto(Produto produto) {
+		this.produto = produto;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+	@PostConstruct
+	public void iniciar() {
+		produtoDAO = new ProdutoDAO();
+	}
+
+	public void listar() {
+		try {
+			ProdutoDAO produtoDAO = new ProdutoDAO();
+			produtos = produtoDAO.listar();
+
+		} catch (RuntimeException e) {
+			Messages.addGlobalError("Ocorreu um erro Ao listar os Produtos");
+			e.printStackTrace();
+		}
+
 	}
 
 }
